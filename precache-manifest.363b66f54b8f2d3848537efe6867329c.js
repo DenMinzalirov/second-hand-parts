9d9fb631cb3a33dfa49b209c5b822d71f012ca8d
Den Minzakirov <Den.Minzakirov@gmail.com>
2020-05-18 09:25:20 +0300

    deploy gh

---
+++ precache-manifest.363b66f54b8f2d3848537efe6867329c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c43a0e0094661688e2e843ddadd743",
+    "url": "/index.html"
+  },
+  {
+    "revision": "855e6462bedd467a7939",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "309b5038a10195404e26",
+    "url": "/static/css/main.5d694c02.chunk.css"
+  },
+  {
+    "revision": "855e6462bedd467a7939",
+    "url": "/static/js/2.24c3c19b.chunk.js"
+  },
+  {
+    "revision": "d62319261eeaed8a861630bfc0c875b2",
+    "url": "/static/js/2.24c3c19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309b5038a10195404e26",
+    "url": "/static/js/main.68d9d71f.chunk.js"
+  },
+  {
+    "revision": "4d167587a8b0ae44d75f",
+    "url": "/static/js/runtime-main.6acbb46e.js"
+  }
+]);
--- src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { userReducer } from './user'
-import { dataBaseReducer } from './dataBase'
-
-export const rootReducer = combineReducers({
-  userInfo: userReducer,
-  dataBase: dataBaseReducer,
-})
